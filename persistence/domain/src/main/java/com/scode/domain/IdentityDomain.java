595b7bf34d1622ffa4ba0352586f697b4355f5d5
santanu <santanu_cst@yahoo.com>
2020-08-27 10:38:43 +0530

    added changes

--- persistence/domain/src/main/java/com/scode/domain/IdentityDomain.java
+++
@@ -1,9 +0,0 @@
-//package com.scode.domain;
-//
-//public interface IdentityDomain {
-//    public GeneratedTokenModel validateUser(AuthRequestModel authRequestModel);
-//
-//    public AuthValidationDTO validateToken(String token, String api, String action);
-//
-//    public GeneratedTokenModel refreshToken(TokenConfigDTO tokenConfig);
-//}
--- persistence/domain/src/main/java/com/scode/domain/ProductDomain.java
+++
@@ -1,9 +0,0 @@
-package com.scode.domain;
-
-import com.scode.domain.model.UserModel;
-
-import java.util.List;
-
-public interface ProductDomain {
-    public List<UserModel> getAll();
-}
